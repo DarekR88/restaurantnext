a8b4c1ab4d1f07d6662046316da43781197a1781
Darek Radke <dradmclad@gmail.com>
2023-12-13 13:50:57 -0600

    navbar and footer setup and css

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export function Footer() {
   return (
     <>
-      <div className="relative bottom-0 flex flex-col-reverse justify-center items-center w-screen py-6 bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-gray-100 via-gray-300 to-gray-500 lg:flex-row lg:px-32 lg:justify-between lg:h-24">
+      <div className="relative bottom-0 flex flex-col-reverse justify-center items-center w-screen py-6 lg:flex-row lg:px-32 lg:justify-between lg:h-24">
         <p>
           <span>&copy;</span> 2023 Darek Radke
         </p>
