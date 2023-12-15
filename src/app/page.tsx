ed061622c7ab7ea5e18527fe812bdc660bc3588c
Darek Radke <dradmclad@gmail.com>
2023-12-15 13:17:17 -0600

    css for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,17 @@ import DiningImg from "../../public/images/carlos-lindner-sjBYA8dAw54-unsplash.j
 export default function Home() {
   return (
     <>
-      <div className="w-screen static flex justify-center">
+      <div className="w-screen static flex justify-center mt-6">
         <div className="max-w-1200px flex flex-col lg:flex-row">
-          <div className="w-screen lg:w-[600px]">
-            <Image src={DiningImg} alt="dining" className="relative lg:w-[450px] lg:h-[285px] z-10" />
-            <div className="relative -top-[200px] left-[85px] lg:w-[450px] lg:h-[285px] bg-bg-tertiary z-0"></div>
+          <div className="w-screen lg:w-[600px] lg:h-[370px] lg:overflow-hidden">
+            <Image src={DiningImg} alt="dining" className="relative lg:w-[500px] lg:h-[285px] z-10" />
+            <div className="relative -top-[225px] left-[60px] lg:w-[500px] lg:h-[285px] bg-bg-tertiary z-0"></div>
           </div>
-          <div className="w-screen lg:w-[600px] lg:pt-[20px]">
-            <h1 className="text-6xl font-Arizonia">Timeless Flavors</h1>
+          <div className="w-screen flex flex-col lg:w-[600px] justify-center">
+            <h1 className="text-6xl font-Arizonia z-10">Timeless Flavors</h1>
             <p className="text-xl font-Open font-thin lg:pl-5">
               Butter, cheese, sugar, salt, sweet, savory. Experience the highest quality
-              ingredients cooked to the highest degree of excellence.
+              ingredients cooked to a degree of excellence you can't find anywhere else.
             </p>
           </div>
         </div>
